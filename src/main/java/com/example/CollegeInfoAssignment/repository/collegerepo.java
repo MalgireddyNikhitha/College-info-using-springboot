b5e1f05ae90dbee06fcf148fe335863e92f49465
Nikhitha Malgireddy <103202516+MalgireddyNikhitha@users.noreply.github.com>
2024-03-01 22:49:36 +0530

    Update collegerepo.java

--- src/main/java/com/example/CollegeInfoAssignment/repository/collegerepo.java
+++ src/main/java/com/example/CollegeInfoAssignment/repository/collegerepo.java
@@ -1,12 +1,24 @@
 package com.example.CollegeInfoAssignment.repository;
 
 import com.example.CollegeInfoAssignment.model.College;
+import com.example.CollegeInfoAssignment.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 
-public interface collegerepo extends JpaRepository<College,Integer> {
+public interface collegerepo extends JpaRepository<College,Long> {
 
-    College findByCollegename(String collegename);
+
+    List<Department> findByname(String name);
+
+
+    College findByName(String name);
+
+
+
+
 }
+
