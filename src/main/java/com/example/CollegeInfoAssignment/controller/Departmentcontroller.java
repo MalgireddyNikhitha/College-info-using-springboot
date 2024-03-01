89523dbfb11819dc580236050e4b4c620fa78250
Nikhitha Malgireddy <103202516+MalgireddyNikhitha@users.noreply.github.com>
2024-03-01 22:44:33 +0530

    Update Departmentcontroller.java

--- src/main/java/com/example/CollegeInfoAssignment/controller/Departmentcontroller.java
+++ src/main/java/com/example/CollegeInfoAssignment/controller/Departmentcontroller.java
@@ -3,23 +3,27 @@ package com.example.CollegeInfoAssignment.controller;
 import com.example.CollegeInfoAssignment.model.Department;
 import com.example.CollegeInfoAssignment.service.Departmentservice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/departments")
+@RequestMapping("/Department")
 public class Departmentcontroller {
     @Autowired
     private Departmentservice departmentService;
+
     @PostMapping
-    public ResponseEntity<Department> addDepartment(@RequestBody Department department) {
-        return ResponseEntity.ok(departmentService.addDepartment(department));
+    public Department addDepartment(@RequestBody Department department) {
+        Department addedDepartment = departmentService.addDepartment(department);
+        return addedDepartment;
     }
 
-    @GetMapping("/collegename/{collegename}")
-    public ResponseEntity<List<Department>> getDepartmentsByCollegename(@PathVariable String collegename) {
-        return ResponseEntity.ok(departmentService.getDepartmentsByCollegename(collegename));
+    @GetMapping("/college/{collegeName}")
+    public List<Department> getDepartmentsByCollegeName(@PathVariable String collegeName) {
+        List<Department> departments = departmentService.getDepartmentsByCollegeName(collegeName);
+        return departments;
     }
+
+
 }
