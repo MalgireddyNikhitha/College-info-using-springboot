049808e09a4440cd923bdc36ad73ecaa20e43fc5
Nikhitha Malgireddy <103202516+MalgireddyNikhitha@users.noreply.github.com>
2024-03-01 22:50:35 +0530

    Update departmentrepo.java

--- src/main/java/com/example/CollegeInfoAssignment/repository/departmentrepo.java
+++ src/main/java/com/example/CollegeInfoAssignment/repository/departmentrepo.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface departmentrepo extends JpaRepository<Department, Integer>{
-    List<Department> findByCollegename(String collegename);
+    List<Department> findByCollegeName(String collegeName);
 }
+
